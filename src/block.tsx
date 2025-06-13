77750742de7ce581b742e2a4d4b31919cd65c48e
Fabien Sabatié <fabien.sabatie@live.fr>
2025-06-13 19:52:02 +0200

    First commit 🔥

---
+++ src/block.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// Define the props interface for this block
+// This interface will be automatically parsed by the MEXT system
+// when you push files to GitHub via Sandpack editor
+interface BlockProps {
+  
+}
+
+
+
+const Block: React.FC<BlockProps> = (props) => {
+  
+  return (
+    <div>
+      {/* start coding here */}
+    </div>
+  );
+};
+
+export default Block;
